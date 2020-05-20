e1ee45941de5dc71f5ef2c8450e7e6af31a57d4b
Kamen <chenkaimin@rfchina.com>
2020-05-20 23:56:36 +0800

    集成j2cache二级缓存框架

--- src/main/java/com/base/backend/config/Config.java
+++ src/main/java/com/base/backend/config/Config.java
@@ -1,10 +1,7 @@
 package com.base.backend.config;
 
 import com.base.backend.core.weixin.config.WeiXinProperties;
-import com.jarvis.cache.serializer.HessianSerializer;
-import com.jarvis.cache.serializer.ISerializer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -19,8 +16,4 @@ import org.springframework.context.annotation.Configuration;
 @EnableConfigurationProperties({WeiXinProperties.class})
 public class Config {
 
-    @Bean
-    public ISerializer<Object> autoloadCacheSerializer() {
-        return new HessianSerializer();
-    }
 }
